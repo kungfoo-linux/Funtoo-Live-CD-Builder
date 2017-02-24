1f3daed24d4c849b0c843e8f4e38448eb3187be3
Daniel <danielkot@users.noreply.github.com>
2017-02-24 22:02:51 +0100

    small changes

--- build.sh
+++ build.sh
@@ -9,49 +9,45 @@ i?86) export url_of_stage_file=http://build.funtoo.org/funtoo-current/x86-32bit/
 x86_64) export url_of_stage_file=http://build.funtoo.org/funtoo-current/x86-64bit/generic_64/stage3-latest.tar.xz ;;
 esac
 
-wget --no-check-cert -c ${url_of_stage_file} -O stage.tar.xz || exit
+wget --no-check-cert -c ${url_of_stage_file} -O stage.tar.xz
 mkdir -p rootfs
 mkdir -p out
-if [ -e rootfs/* ]; then
-# DO NOTHING!
-else
-tar -xf stage.tar.xz -C rootfs || exit
-fi
+[ -d rootfs/boot ] && true || tar -xvf stage.tar.xz -C rootfs
 cd rootfs
 mkdir -p dev sys proc
-mount --bind /dev dev || {umount -f dev proc sys; exit}
-mount --bind /sys sys || {umount -f dev proc sys; exit}
-mount --bind /proc proc || {umount -f dev proc sys; exit}
-cp /etc/resolv.conf etc || {umount -f dev proc sys; exit}
-chroot . emerge --sync || exit
-chroot . epro mix-ins +xfce || {umount -f dev proc sys; exit}
-chroot . echo "exec startxfce4 --with-ck-launch" > ~/.xinitrc || {umount -f dev proc sys; exit}
-chroot . emerge boot-update wicd squashfs-tools opera-developer geany porthole xorg-x11 dialog cdrtools lightdm genkernel xfce4-meta || {umount -f dev proc sys; exit}
-chroot . rc-update add consolekit default || {umount -f dev proc sys; exit}
-chroot . rc-update add dhcpcd default || {umount -f dev proc sys; exit}
-chroot . echo "DISPLAYMANAGER='lightdm'" > /etc/conf.d/xdm || {umount -f dev proc sys; exit}
-chroot . rc-update add xdm default || {umount -f dev proc sys; exit}
-chroot . rc-update add dbus default || {umount -f dev proc sys; exit}
-chroot . rm -rf /usr/src/* || {umount -f dev proc sys; exit}
-chroot . rm -rf /boot || {umount -f dev proc sys; exit}
-chroot . mkdir -p /usr/src/linux || {umount -f dev proc sys; exit}
-chroot . mkdir -p /boot || {umount -f dev proc sys; exit}
-chroot . rm -rf /lib/modules/* || {umount -f dev proc sys; exit}
-chroot . rm -rf /lib64/modules/* || {umount -f dev proc sys; exit}
+mount --bind /dev dev
+mount --bind /sys sys
+mount --bind /proc proc
+cp /etc/resolv.conf etc
+chroot . emerge --sync
+chroot . epro mix-ins +xfce
+chroot . echo "exec startxfce4 --with-ck-launch" > ~/.xinitrc
+chroot . emerge boot-update wicd squashfs-tools opera-developer geany porthole xorg-x11 dialog cdrtools lightdm genkernel xfce4-meta
+chroot . rc-update add consolekit default
+chroot . rc-update add dhcpcd default
+chroot . echo "DISPLAYMANAGER='lightdm'" > /etc/conf.d/xdm
+chroot . rc-update add xdm default
+chroot . rc-update add dbus default
+chroot . rm -rf /usr/src/*
+chroot . rm -rf /boot
+chroot . mkdir -p /usr/src/linux
+chroot . mkdir -p /boot
+chroot . rm -rf /lib/modules/*
+chroot . rm -rf /lib64/modules/*
 echo "
 Please provide a 'root' password:
 "
-chroot . passwd root || {umount -f dev proc sys; exit}
+chroot . passwd root
 cd ..
-cp -raf /usr/src/linux/* rootfs/usr/src/linux || {umount -f dev proc sys; exit}
-cp -raf `readlink -f /vmlinuz` rootfs/boot/vmlinuz || {umount -f dev proc sys; exit}
-cp -raf /lib/modules/`uname -r` rootfs/lib/modules/`uname -r` || {umount -f dev proc sys; exit}
-cp -raf stage/* rootfs || {umount -f dev proc sys; exit}
+cp -raf /usr/src/linux/* rootfs/usr/src/linux
+cp -raf `readlink -f /vmlinuz` rootfs/boot/vmlinuz
+cp -raf /lib/modules/`uname -r` rootfs/lib/modules/`uname -r`
+cp -raf stage/* rootfs
 cd rootfs
-chroot . /tmp/linx-live/build || {umount -f dev proc sys; exit}
-umount -f dev proc sys || {umount -f dev proc sys; exit}
+chroot . /tmp/linx-live/build
+umount -f dev proc sys
 cd ..
-mv -f rootfs/*.iso rootfs/*.zip out || {umount -f dev proc sys; exit}
+mv -f rootfs/*.iso rootfs/*.zip out
 clear
 echo "
 ALL GOOD FIND IMAGES IN `pwd`/out DIR :)
@@ -60,15 +56,7 @@ ls out | sort
 }
 
 case $1 in
-build)	build_ || echo "
-!!!ERROR FOUND!!!
-!!!ERROR FOUND!!!
-!!!ERROR FOUND!!!
-!!!ERROR FOUND!!!
-!!!ERROR FOUND!!!
-!!!ERROR FOUND!!!
-!!!ERROR FOUND!!!
-" && {umount -f dev proc sys; exit} ;;
+build)	build_ || clear && echo "!!!ERROR FOUND!!!" && umount -f dev proc sys && exit ;;
 clean)	rm -rf rootfs out stage.tar.xz && exit ;;
 *)		clear && echo "
 Only use $0 <build|clean>!
