78e44ffd32462410ff85b94c7634331b3be5cb52
Daniel <danielkot@users.noreply.github.com>
2017-06-24 13:24:36 +0200

    Add arch detection

--- build.sh
+++ build.sh
@@ -57,8 +57,8 @@ NOTE: On 32-bit host machine you can't use a 64-bit for building.
 done
 
 case `cat .asked_arch.cfg` in
-1) export url_of_stage_file=http://build.funtoo.org/funtoo-current/x86-32bit/generic_32/stage3-latest.tar.xz ; export portage_make_dot_conf=/usr/share/portage/make.conf.i686 ;;
-2) export url_of_stage_file=http://build.funtoo.org/funtoo-current/x86-64bit/generic_64/stage3-latest.tar.xz ; export portage_make_dot_conf=/usr/share/portage/make.conf.x86_64 ;;
+1) export url_of_stage_file=http://build.funtoo.org/funtoo-current/x86-32bit/generic_32/stage3-latest.tar.xz ; export portage_make_dot_conf=/usr/share/portage/make.conf.i686 ; export lnx=linux32 ;;
+2) export url_of_stage_file=http://build.funtoo.org/funtoo-current/x86-64bit/generic_64/stage3-latest.tar.xz ; export portage_make_dot_conf=/usr/share/portage/make.conf.x86_64 ; export lnx=linux64 ;;
 *) die "Architecture is'nt supported or unkown option '`cat .asked_arch.cfg`' !" ;;
 esac
 
@@ -86,7 +86,7 @@ compare_
 if [ ! -e './stamps/01' ]; then
 	if ! (
 		touch './stamps/01'
-		chroot rootfs emerge --sync
+		chroot rootfs ${lnx} emerge --sync
 	); then
 		die "Can't sync the portage" '01'
 	fi
@@ -95,8 +95,8 @@ fi
 if [ ! -e './stamps/02' ]; then
 	if ! (
 		touch './stamps/02'
-		chroot rootfs epro flavor desktop
-		chroot rootfs epro mix-ins +xfce
+		chroot rootfs ${lnx} epro flavor desktop
+		chroot rootfs ${lnx} epro mix-ins +xfce
 	); then
 		die "Can't setup mix-ins!" '02'
 	fi
@@ -105,7 +105,7 @@ fi
 if [ ! -e './stamps/03' ]; then
 	if ! (
 		touch './stamps/03'
-		chroot rootfs echo "exec startxfce4 --with-ck-launch" > ~/.xinitrc
+		chroot rootfs ${lnx} echo "exec startxfce4 --with-ck-launch" > ~/.xinitrc
 	); then
 		die "Can't setup xinitrc!" '03'
 	fi
@@ -114,19 +114,19 @@ fi
 if [ ! -e './stamps/04' ]; then
 	if ! (
 		cp -raf stage/* rootfs
-		chroot rootfs rm -rf /etc/motd
-		chroot rootfs rm -rf /etc/portage/make.conf*
-		chroot rootfs ln -sf ${portage_make_dot_conf} /etc/portage/make.conf
-		chroot rootfs ln -sf ${portage_make_dot_conf} /etc/portage/make.conf.example
-		chroot rootfs chmod 7777 /tmp
+		chroot rootfs ${lnx} rm -rf /etc/motd
+		chroot rootfs ${lnx} rm -rf /etc/portage/make.conf*
+		chroot rootfs ${lnx} ln -sf ${portage_make_dot_conf} /etc/portage/make.conf
+		chroot rootfs ${lnx} ln -sf ${portage_make_dot_conf} /etc/portage/make.conf.example
+		chroot rootfs ${lnx} chmod 7777 /tmp
 		touch './stamps/04'
-		chroot rootfs emerge -uvDN --ask n --with-bdeps=y @world
-		chroot rootfs emerge boot-update wicd squashfs-tools opera-developer geany porthole xorg-x11 dialog cdrtools lightdm genkernel xfce4-meta --autounmask --autounmask-write --verbose --ask n
-		chroot rootfs etc-update <<!
+		chroot rootfs ${lnx} emerge -uvDN --ask n --with-bdeps=y @world
+		chroot rootfs ${lnx} emerge boot-update wicd squashfs-tools opera-developer geany porthole xorg-x11 dialog cdrtools lightdm genkernel xfce4-meta --autounmask --autounmask-write --verbose --ask n
+		chroot rootfs ${lnx} etc-update <<!
 -3
 !
 		#	Now we must repeat above command for some reasons to 'autounmask' masked packages!
-		chroot rootfs emerge boot-update wicd squashfs-tools opera-developer geany porthole xorg-x11 dialog cdrtools lightdm genkernel xfce4-meta --autounmask --autounmask-write --verbose --ask n
+		chroot rootfs ${lnx} emerge boot-update wicd squashfs-tools opera-developer geany porthole xorg-x11 dialog cdrtools lightdm genkernel xfce4-meta --autounmask --autounmask-write --verbose --ask n
 	); then
 		die "Can't emerge default packages!" '04'
 	fi
@@ -135,11 +135,11 @@ fi
 if [ ! -e './stamps/05' ]; then
 	if ! (
 		touch './stamps/05'
-		chroot rootfs rc-update add consolekit default
-		chroot rootfs rc-update add dhcpcd default
-		chroot rootfs echo "DISPLAYMANAGER='lightdm'" > /etc/conf.d/xdm
-		chroot rootfs rc-update add xdm default
-		chroot rootfs rc-update add dbus default
+		chroot rootfs ${lnx} rc-update add consolekit default
+		chroot rootfs ${lnx} rc-update add dhcpcd default
+		chroot rootfs ${lnx} echo "DISPLAYMANAGER='lightdm'" > /etc/conf.d/xdm
+		chroot rootfs ${lnx} rc-update add xdm default
+		chroot rootfs ${lnx} rc-update add dbus default
 	); then
 		die "Can't setup rc-update!" '05'
 	fi
@@ -151,7 +151,7 @@ if [ ! -e './stamps/06' ]; then
 #	echo "
 #Please provide a 'root' password:
 #	"
-		chroot rootfs passwd root <<!
+		chroot rootfs ${lnx} passwd root <<!
 toor
 toor
 !
@@ -164,9 +164,9 @@ toor
 	fi
 fi
 
-chroot rootfs rm -rf /usr/portage/distfiles/*
+chroot rootfs ${lnx} rm -rf /usr/portage/distfiles/*
 
-if chroot rootfs /tmp/linx-live/build; then
+if chroot rootfs ${lnx} /tmp/linx-live/build; then
 	umount_
 	mv -f rootfs/*.iso rootfs/*.zip out
 	clear
@@ -184,7 +184,7 @@ fi
 
 case ${1} in
 build)	build ;;
-chroot)	mount_ && compare_ && chroot rootfs && umount_ ;;
+chroot)	mount_ && compare_ && chroot rootfs ${lnx} && umount_ ;;
 clean)	rm -rf rootfs out stage.tar.xz stamps .asked_arch.cfg ;;
 clean-variable)	rm -rf .asked_arch.cfg ;;
 *)	clear ; echo -e "\nOnly use:\n`basename $0` <build|clean|chroot|clean-variable>\n" ;;
